85c41c2a4d6ae3b6278404496393026bdaafc45b
Sujoy Datta <sujoydatta26@gmail.com>
2019-07-09 14:47:59 +0530

    changed name of project to starbase added character components

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+
+    <Character :id="1"/>
+   </div>
+</template>
+
+<script>
+  import Character from './components/Character.vue'
+  export default {
+    name: 'app',
+    data(){
+      return{
+        title: 'Generate your team'
+      }
+    },
+    components: {
+      Character
+    }
+  }
+</script>
---
+++ applications/starbase/src/components/Character.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="">
+    {{character}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['id'],
+    data(){
+      return {
+        character: {}
+      }
+    },
+    methods:{
+      fetchCharacter(id){
+        var apiString  = "https://swapi.co/api/people/" + id
+        fetch(apiString, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => this.character = json)
+      }
+    },
+    created(){
+      this.fetchCharacter(this.id)
+    }
+  }
+</script>
